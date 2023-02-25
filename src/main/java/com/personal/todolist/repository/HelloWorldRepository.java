093a009bc300c7b9e77a1a8994c83fe26eab6d93
KamJer <104097463+KamJer@users.noreply.github.com>
2023-02-25 11:25:02 +0100

    Revert "Create new mem database and get data from it"

--- src/main/java/com/personal/todolist/repository/HelloWorldRepository.java
+++
@@ -1,8 +0,0 @@
-package com.personal.todolist.repository;
-
-import com.personal.todolist.entity.HelloWorldEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HelloWorldRepository extends JpaRepository<HelloWorldEntity, Long> {
-
-}
